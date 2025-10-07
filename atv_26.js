d5bf456f3d0574fc6065b952851b44f9e301817a
Gloria Oliveira <gloriost@gmail.com>
2025-10-06 21:18:47 -0300

    primeiro commit

---
+++ atv_26.js
@@ -0,0 +1,8 @@
+function multiplicacao(a, b) {
+    return a * b;
+}
+console.log(multiplicacao(2, 3));
+function concat(nome) {
+    return "Ol\u00E1, ".concat(nome);
+}
+console.log(concat("João"));
---
+++ atv_26.ts
@@ -0,0 +1,11 @@
+function multiplicacao(a: number, b: number): number{
+    return a*b
+}
+
+console.log(multiplicacao(2,3))
+
+function concat(nome: string): string {
+    return `Olá, ${nome}`
+}
+
+console.log(concat("João"))
